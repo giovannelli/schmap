13537a13879152264e546de4422b7fbf06ad979c
Duccio Giovannelli <giovannelli@extendi.it>
2012-10-30 17:42:06 +0100

    Travis

--- spec/schmap_spec.rb
+++ spec/schmap_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Schmap::SchmapApi do
-  it "It should return user 12300001 and 12300002 info" do
+  
+  it "Passing twitter ids should return users info" do
     ids = ["12300001", "12300002"]
     client = Schmap::SchmapApi.new
     users = client.list_of_twitter_ids(ids)
@@ -9,4 +10,7 @@ describe Schmap::SchmapApi do
       user[:twitter_id].should == ids[i]
     end
   end
+  
+
+  
 end
